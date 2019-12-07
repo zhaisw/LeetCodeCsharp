637353c61446854ec0d88d457a7769c80d3bee36
zhaisw <shuwei.zhai@gmail.com>
2019-12-07 13:02:58 +1300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Q1: Two Sum
+            var mySolution = new Solution();
+            var indices = mySolution.TwoSum(new[] { 2, 10, 7, 11, 9 }, 9);
+            try
+            {
+                Console.WriteLine("Result of TwoSum is: [{0}, {1}]", indices[0], indices[1]);
+            }
+            catch
+            {
+                Console.WriteLine("No two sum match for target.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCodeCsharp
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] numberList, int target)
+        {
+
+            for (int i = 0; i < numberList.Length; i++)
+            {
+                for (int j = i + 1; j < numberList.Length; j++)
+                {
+                    if (numberList[j] == target - numberList[i])
+                    {
+                        return new int[2] { i, j };
+                    }
+                }
+            }
+
+            return new int[] { };
+        }
+    }
+}
