f55f5488e891636e3188756a2f8aca778cb24f6e
zhaisw <shuwei.zhai@gmail.com>
2019-12-09 21:35:38 +1300

    Added Q1009, modified Q1.

--- Program.cs
+++ Program.cs
@@ -10,9 +10,12 @@ namespace LeetCodeCsharp
     {
         static void Main(string[] args)
         {
-            // Q1: Two Sum
-            var indices = Solution.TwoSumDict(new[] { 2, 10, 7, 11, 9 }, 9);
+            // Q1: Two Sum - Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+            var indices = Solution.Q1TwoSumDict(new[] { 2, 10, 17, 7, 9 }, 9);
             Console.WriteLine("Result of TwoSum is: [{0}, {1}]", indices[0], indices[1]);
+
+            // Q1009: Complement of Base 10 Integer
+            Console.WriteLine("Complement of int {0} is: {1}", 10, Solution.Q1009BitwiseComplement(10));
 
             Console.ReadKey();
         }
--- Solution.cs
+++ Solution.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using static System.Math;
 
 namespace LeetCodeCsharp
 {
     public class Solution
     {
-        public static int[] TwoSum(int[] numberList, int target)
+        public static int[] Q1TwoSum(int[] numberList, int target)
         {
             for (int i = 0; i < numberList.Length; i++)
             {
@@ -22,18 +23,43 @@ namespace LeetCodeCsharp
             return null;
         }
 
-        public static int[] TwoSumDict(int[] nums, int target)
+        public static int[] Q1TwoSumDict(int[] nums, int target)
         {
-            var dict = new Dictionary<int, int>();
+            var numsDict = new Dictionary<int, int>(); // try to use dictionary for index purpose.
 
             for (int i = 0; i < nums.Length; i++)
             {
-                if (dict.ContainsKey(target - nums[i]))
-                    return new int[] { dict[target - nums[i]], i };
-                dict[nums[i]] = i;
+                if (!numsDict.ContainsKey(target - nums[i]))
+                {
+                    numsDict[nums[i]] = i;
+                }
+                else
+                {
+                    return new int[2] { numsDict[target - nums[i]], i};
+                }
             }
 
             return null;
+        }
+
+        public static int Q1009BitwiseComplement(int N)
+        {
+            var complementInt = 0;
+            var cnt = 0;
+
+            if (N == 0) return 1;
+
+            while (N > 0)
+            {
+                if (N % 2 == 0)
+                {
+                    complementInt += Convert.ToInt32(Pow(2, cnt));
+                }
+                cnt += 1;
+                N /= 2;
+            }
+
+            return complementInt;
         }
     }
 }
